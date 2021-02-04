46e4e2fab17ce9849d92da09213875171146ed9c
Carlton Joseph <carlton.joseph@gmail.com>
2021-02-03 17:55:55 -0800

    add css scss postcss browserslist hotreloading

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Webpack 5
 The code in this repository is based on
 [Webpack 5 Full Project Setup](https://youtu.be/TOb1c39m64A)
 video.
+
+## Notes
+
+- `target` in webpack.config.js use to allow HMR in **dev** mode via **web**
+  - due to a bug
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,27 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const mode = process.env.NODE_ENV || "development";
+const prod = process.env.NODE_ENV && process.env.NODE_ENV === "production";
 
 module.exports = {
-  mode,
+  mode: prod ? "production" : "development",
+  target: prod ? "browserslist" : "web",
   devtool: "source-map",
   devServer: {
     contentBase: path.join(__dirname, "dist"),
+    hot: true,
   },
   module: {
     rules: [
+      {
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+        test: /\.s?css$/i,
+      },
       {
         use: { loader: "babel-loader" },
         test: /\.js$/,
@@ -17,4 +29,5 @@ module.exports = {
       },
     ],
   },
+  plugins: [new MiniCssExtractPlugin()],
 };
